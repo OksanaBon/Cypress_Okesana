847d064d8570b3d61a9e5efd571b326fc64f0341
Ksushenka Bondar <ksushenkabondar@gmail.com>
2023-11-22 17:25:29 +0100

    coment to new-lessons

---
+++ cypress/e2e/test.js
@@ -0,0 +1,7 @@
+const originalArray = [1, 2, 3, 4, 5, 2, 6, 7];
+const valueToRemove = 2; // Значення, яке потрібно видалити
+
+const filteredArray = originalArray.filter(item => item !== valueToRemove);
+
+console.log(filteredArray);
+WebAssembly jnkmk,
